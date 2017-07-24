d0f073406c542d71f9f9da77de0476ee1845c0d7
ameya-woxi <ameya.woxi@gmail.com>
2017-07-24 10:17:17 +0530

    Removed Quantity

--- resources/views/admin/bill/view.blade.php
+++ resources/views/admin/bill/view.blade.php
@@ -439,67 +439,7 @@
         });
         $('select[name="change_bill"]').find('option[value={{$selectedBillId}}]').attr("selected",true);
 
-        $("#billTransactionCreateButton").on('click',function(e){
-            e.stopPropagation();
-            $("#billTransactionListingTab").removeClass('active');
-            $("#billTransactionCreateTab").addClass('active');
-        });
 
-        $("#transactionSubmit").on('click',function(e){
-            e.stopPropagation();
-            console.log('in submit');
-            var formData = $("#createTransactionForm").serializeArray();
-            $.ajax({
-                url: '/bill/transaction/create',
-                type: 'POST',
-                async: false,
-                data: formData,
-                success: function(data,textStatus,xhr){
-                    $("#billTransactionCreateTab").removeClass('active');
-                    $("#billTransactionListingTab").addClass('active');
-                },
-                error: function(data){
-
-                }
-            });
-        });
-    });
-
-    typingTimer = 0;
-    doneTypingInterval = 1000;
-    $("#transactionTotal").on('keyup', function () {
-        clearTimeout(typingTimer);
-        typingTimer = setTimeout(calculateTransactionDetails, doneTypingInterval);
-    });
-    $("#transactionTotal").on('keydown', function () {
-        clearTimeout(typingTimer);
-    });
-
-    function calculateTransactionDetails(){
-        var billId = $("#billId").val();
-        var total = $("#transactionTotal").val();
-        $.ajax({
-            url: '/bill/calculate-tax-amounts',
-            type: 'POST',
-            async: false,
-            data:{
-                _token: $("input[name='_token']").val(),
-                bill_id: billId,
-                total: total
-            },
-            success: function(data,textStatus,xhr){
-                $("#transactionSubTotal").val(data.subtotal);
-                console.log(data.taxes);
-                $.each(data.taxes, function(i,v){
-                    console.log(i);
-                    console.log(v);
-                    $("#TaxAmount_"+ v.tax_id).val(v.tax_amount);
-                })
-            },
-            error: function(data){
-
-            }
-        });
 
 
     }
