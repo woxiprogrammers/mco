e1edf723b2b7714444cc5aae4b78545e941c2c2b
ameya-woxi <ameya.woxi@gmail.com>
2017-08-20 16:29:13 +0530

    Extra item in quotation is completed

--- public/assets/custom/admin/quotation/validations.js
+++ public/assets/custom/admin/quotation/validations.js
@@ -127,3 +127,59 @@ var  EditQuotation = function () {
         }
     };
 }();
+
+var  WorkOrderFrom = function () {
+    var handleCreate = function() {
+        var form = $('#WorkOrderForm');
+        var error = $('.alert-danger', form);
+        var success = $('.alert-success', form);
+        form.validate({
+            errorElement: 'span', //default input error message container
+            errorClass: 'help-block', // default input error message class
+            focusInvalid: false, // do not focus the last invalid input
+            rules: {
+
+            },
+
+            messages: {
+
+            },
+
+            invalidHandler: function (event, validator) { //display error alert on form submit
+                success.hide();
+                error.show();
+            },
+
+            highlight: function (element) { // hightlight error inputs
+                $(element)
+                    .closest('.form-group').addClass('has-error'); // set error class to the control group
+            },
+
+            unhighlight: function (element) { // revert the change done by hightlight
+                $(element)
+                    .closest('.form-group').removeClass('has-error'); // set error class to the control group
+
+            },
+
+            success: function (label) {
+                label
+                    .closest('.form-group').addClass('has-success');
+            },
+
+            submitHandler: function (form) {
+                $("button[type='submit']").attr('disabled', true);
+                if (confirm("Please confirm work order details and extra items' costs.") == true) {
+                    form.submit();
+                }
+                success.show();
+                error.hide();
+            }
+        });
+    }
+
+    return {
+        init: function () {
+            handleCreate();
+        }
+    };
+}();
