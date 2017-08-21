7868e8fa5423fd64b782e2e5e4f6054775be6b06
Aishwarya Nair <aishwarya.woxi@gmail.com>
2017-08-21 18:38:42 +0530

    Vendors Migration Done

---
+++ database/migrations/2017_08_21_180258_create_vendors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVendorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name',255);
+            $table->string('company');
+            $table->integer('mobile');
+            $table->string('email');
+            $table->string('gstin');
+            $table->integer('alternate_contact')->nullable();
+            $table->string('city');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendors');
+    }
+}
