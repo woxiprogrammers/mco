02e72a8efe43a31c2770a8eacd90f9ab7ae837b6
ameya-woxi <ameya.woxi@gmail.com>
2017-08-28 11:31:04 +0530

    added foreign key code in purchase request migration

--- database/migrations/2017_08_26_165227_create_purchase_requests_table.php
+++ database/migrations/2017_08_26_165227_create_purchase_requests_table.php
@@ -20,6 +20,13 @@ class CreatePurchaseRequestsTable extends Migration
             $table->unsignedInteger('user_id');
             $table->unsignedInteger('behalf_of_user_id');
             $table->unsignedInteger('purchase_component_status_id');
+            $table->foreign('quotation_id')->references('id')->on('quotations')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('project_site_id')->references('id')->on('project_sites')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('behalf_of_user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('purchase_component_status_id')->references('id')->on('purchase_request_component_statuses')->onDelete('cascade')->onUpdate('cascade');
+
+
             $table->timestamps();
         });
     }
