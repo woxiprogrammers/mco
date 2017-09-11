54c77b816185811512f8811bd69ab0008c8ab55a
unknown <shivani.woxi@gmail.com>
2017-09-11 11:17:16 +0530

    UI Changes Done

--- resources/views/admin/checkList/create.blade.php
+++ resources/views/admin/checkList/create.blade.php
@@ -20,7 +20,7 @@
                                     <h1>Create CheckList</h1>
                                 </div>
                                 <div class="form-group " style="float: right;margin-top:1%">
-                                    <a href="#" class="btn btn-set red pull-right">
+                                    <a href="/checkList/manage" class="btn btn-set red pull-right">
                                         <i class="fa fa-check"></i>
                                         Submit
                                     </a>
@@ -42,7 +42,7 @@
                                                     <div class="form-group row">
                                                         <div class="row">
 
-                                                                <div class="col-md-5" style="text-align: right; margin-left: -6%">
+                                                                <div class="col-md-5" style="text-align: right; margin-left: -6% ; font-size: 14px">
                                                                     <label for="main_cat" class="control-label">Select Main Category Here</label>
                                                                     <span>*</span>
                                                                 </div>
@@ -54,7 +54,7 @@
 
                                                                     <br>
                                                             <br>
-                                                                        <div class="col-md-5" style="text-align: right ; margin-left: -6%">
+                                                                        <div class="col-md-5" style="text-align: right ; margin-left: -6% ; font-size: 14px">
                                                                             <label for="sub_cat" class="control-label">Select Sub Category Here</label>
                                                                             <span>*</span>
                                                                         </div>
@@ -66,59 +66,70 @@
                                                         </div>
                                                     </div>
 
-                                                    <div class="col-md-5" style="text-align: right ; margin-left: -6.6%">
+                                                    <div class="col-md-5" style="text-align: right ; margin-left: -6.6% ; font-size: 14px">
 
                                                         <label for="title" class="control-label">Title</label>
                                                         <span>*</span>
                                                     </div>
                                                     <div class="col-md-6">
                                                         <input type="text" class="form-control" id="titlename" placeholder="Enter Title Here">
-                                                        <div id="sample_editable_1_new" class="btn yellow" style="margin-top: -7%; margin-left: 105%"><button style="color: white" id="add"><i class="fa fa-plus"></i> </button>
+                                                        <div id="sample_editable_1_new" class="btn yellow" style="margin-top: -9%; margin-left: 105%"><button style="color: white" id="add"><i class="fa fa-plus"></i> </button>
                                                         </div>
                                                         <div id="removeBtn"  style="margin-top: -5%; margin-left: 118%" >
-
                                                         </div>
                                                     </div>
-                                                    <div class="form-body">
-                                                        <br>
-
-                                                        <div class="form-group row">
-                                                            <div class="col-md-5" style="text-align: right ; margin-left: -6%">
-                                                                <label for="description" class="control-label">Description</label>
-                                                                <span>*</span>
-                                                            </div>
-                                                            <div class="col-md-6">
-                                                                <input type="text" class="form-control" id="description" name="description" placeholder="Enter Description Here">
-                                                            </div>
-                                                        </div>
 
                                                     <div class="form-body">
                                                         <div class="form-group row">
-                                                            <div class="col-md-5" style="text-align: right ; margin-left: -6% ; margin-top: -2%">
-                                                                <label for="no_images" class="control-label">Compulsory Number Of Images</label>
+                                                            <div class="col-md-5" style="text-align: right ; margin-left: -42% ; margin-top: 3% ; font-size: 14px">
+                                                                <label for="no_images" class="control-label"> Number Of Images</label>
                                                                 <span>*</span>
                                                             </div>
-                                                            <div class="col-md-6" style="margin-top: -2%">
-                                                                <input type="text" class="form-control" id="no_images" name="no_images" placeholder="Enter Compulsory Number Of Images Here">
+                                                            <div class="col-md-6" style="margin-top: 3% ;margin-left: -2% ; font-size: 14px">
+                                                                <div class="col-md-6">
+                                                                    <input type="text" id="nochapter" >
+                                                                </div>
                                                             </div>
+                                                            <div class="col-md-6" style="margin-top:-3% ; margin-left: 37%">
+                                                            <input type="button" value="Set" onclick="generate()" >
                                                         </div>
                                                         <div class="form-group row">
                                                         </div>
                                                     </div>
-                                                    <div class="form-group row">
+                                                    <div id="description">
+                                                        <div class="form-body">
                                                         <br>
-                                                        <div class="col-md-5" style="text-align: right ;margin-top: -5%;margin-left: -6%">
-                                                            <label for="is_special" class="control-label" style="text-align: right ;margin-top: -5%;margin-left: 1%">Is Remark Mandatory ?</label>
-                                                            <span>*</span>
+                                                        <div class="form-group row">
+                                                            <div class="col-md-6" style="text-align: right ; margin-left: 12% ; margin-top: -5% ; font-size: 14px" >
+                                                                <label for="is_special" class="control-label" style="text-align: right ">Is Remark Mandatory ?</label>
+                                                                <span>*</span>
+                                                                <label for="description" class="control-label" style=" font-size: 14px ;text-align: left ; margin-left: 23%">Description</label>
+                                                                <span>*</span>
+
+                                                            </div>
+                                                            <div id="extra">
+                                                             <div class="row">
+                                                                 <div class="col-md-6" >
+                                                                     <input type="checkbox" class="make-switch" data-on-text="Yes" data-off-text="No" name="is_special">
+                                                                 </div>
+                                                                 <div class="col-md-6">
+                                                                     <input type="text" class="form-control" id="description" name="description" placeholder="Enter Description Here"  >
+                                                                 </div>
+                                                             </div>
+                                                            </div>
                                                         </div>
-                                                        <div class="col-md-6" style="margin-top: -5%;margin-left: 16%">
-                                                            <input type="checkbox" class="make-switch" data-on-text="Yes" data-off-text="No" name="is_special">
                                                         </div>
-                                                    </div>
+
+                                                        <div class="form-group row">
+                                                            <div id="append">
+
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </div>
                                                 <div id="appendHere" >
 
+                                                </div>
                                                 </div>
                                             </form>
                                         </div>
@@ -142,7 +153,12 @@
     <script src="/assets/global/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js" type="text/javascript"></script>
     <script src="/assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js" type="text/javascript"></script>
     <script src="/assets/custom/purchase/manage-datatable.js" type="text/javascript"></script>
+    <script>
+        $('document').ready(function(){
 
+            $('#extra').hide();
+        })
+    </script>
     <script>
         $('#add').click(function(){
             ($('#example').clone()).appendTo('#appendHere');
@@ -152,7 +168,22 @@
             });
         })
         $('#removeBtn').click(function(){
-            $('#example').remove();
+            $('#appendHere').remove();
         })
     </script>
+    <script>
+        function generate() {
+
+            var a = parseInt(document.getElementById("nochapter").value);
+
+            for (i = 0; i < a; i++) {
+
+                ($('#extra').clone()).appendTo('#append');
+                $('#extra').show();
+
+            }
+        }
+
+    </script>
+
 @endsection
