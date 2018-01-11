dcfb6b7c457a31abfcfcf6ce93fa454a4c563814
Ameya Joshi <ameya.woxi@gmail.com>
2018-01-11 08:38:15 +0000

    Last login related migration and models are created

--- app/User.php
+++ app/User.php
@@ -51,4 +51,8 @@ class User extends Authenticatable
             return true;
         }
     }
+
+    public function lastLogins(){
+        return $this->hasMany('App\UserLastLogin','user_id');
+    }
 }
