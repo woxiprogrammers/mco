b8bab3dcd2f35eeb7df673e37520c138990dfae1
Ameya Joshi <ameya.woxi@gmail.com>
2018-03-15 18:43:24 +0530

    Total in purchase order pdf are completed

--- resources/views/purchase/purchase-request/pdf/vendor-quotation.blade.php
+++ resources/views/purchase/purchase-request/pdf/vendor-quotation.blade.php
@@ -26,6 +26,10 @@
         </style>
     </head>
     <body>
+    @php
+        $totalQuantity = $totalRate = $grandTotal = 0;
+        $totalSubtotal =  $totalCGSTAmount = $totalSGSTAmount = $totalIGSTAmount = 0;
+    @endphp
         <span style="text-align: center; margin-left: 35%; font-size: 19px; font-weight: bold">{{$pdfTitle}}({{$formatId}})</span>
         <table border="1" id="mainTable">
             <tr style="height: 100px">
@@ -174,27 +178,48 @@
                                     </td>
                                     <td>
                                         {{$vendorInfo['materials'][$iterator]['quantity']}}
+                                        @php
+                                            $totalQuantity += $vendorInfo['materials'][$iterator]['quantity'];
+                                        @endphp
                                     </td>
                                     <td>
                                         {{$vendorInfo['materials'][$iterator]['unit']}}
                                     </td>
                                     <td>
                                         {{$vendorInfo['materials'][$iterator]['rate']}}
+                                        @php
+                                            $totalRate += $vendorInfo['materials'][$iterator]['rate'];
+                                        @endphp
                                     </td>
                                     <td>
                                         {{$vendorInfo['materials'][$iterator]['subtotal']}}
+                                        @php
+                                            $totalSubtotal += $vendorInfo['materials'][$iterator]['subtotal'];
+                                        @endphp
                                     </td>
                                     <td>
                                         {{$vendorInfo['materials'][$iterator]['cgst_amount']}}({{$vendorInfo['materials'][$iterator]['cgst_percentage']}}%)
+                                        @php
+                                            $totalCGSTAmount += $vendorInfo['materials'][$iterator]['cgst_amount'];
+                                        @endphp
                                     </td>
                                     <td>
                                         {{$vendorInfo['materials'][$iterator]['sgst_amount']}}({{$vendorInfo['materials'][$iterator]['sgst_percentage']}}%)
+                                        @php
+                                            $totalSGSTAmount += $vendorInfo['materials'][$iterator]['sgst_amount'];
+                                        @endphp
                                     </td>
                                     <td>
                                         {{$vendorInfo['materials'][$iterator]['igst_amount']}}({{$vendorInfo['materials'][$iterator]['igst_percentage']}}%)
+                                        @php
+                                            $totalIGSTAmount += $vendorInfo['materials'][$iterator]['igst_amount'];
+                                        @endphp
                                     </td>
                                     <td>
                                         {{$vendorInfo['materials'][$iterator]['total']}}
+                                        @php
+                                            $grandTotal += $vendorInfo['materials'][$iterator]['total'];
+                                        @endphp
                                     </td>
                                 @else
                                     <td style="text-align: left;padding-left: 5px" >
@@ -202,6 +227,9 @@
                                     </td>
                                     <td>
                                         {{$vendorInfo['materials'][$iterator]['quantity']}}
+                                        @php
+                                            $totalQuantity += $vendorInfo['materials'][$iterator]['quantity'];
+                                        @endphp
                                     </td>
                                     <td>
                                         {{$vendorInfo['materials'][$iterator]['unit']}}
@@ -227,23 +255,38 @@
                                 </td>
                                 <td>
                                     {{$vendorInfo['materials'][$iterator]['transportation_amount']}}
+                                    @php
+                                        $totalSubtotal += $vendorInfo['materials'][$iterator]['transportation_amount'];
+                                    @endphp
                                 </td>
                                 <td>
                                     {{$vendorInfo['materials'][$iterator]['transportation_cgst_amount']}} ({{$vendorInfo['materials'][$iterator]['transportation_cgst_percentage']}}%)
+                                    @php
+                                        $totalCGSTAmount += $vendorInfo['materials'][$iterator]['transportation_cgst_amount'];
+                                    @endphp
                                 </td>
                                 <td>
                                     {{$vendorInfo['materials'][$iterator]['transportation_sgst_amount']}} ({{$vendorInfo['materials'][$iterator]['transportation_sgst_percentage']}}%)
+                                    @php
+                                        $totalSGSTAmount += $vendorInfo['materials'][$iterator]['transportation_sgst_amount'];
+                                    @endphp
                                 </td>
                                 <td>
                                     {{$vendorInfo['materials'][$iterator]['transportation_igst_amount']}} ({{$vendorInfo['materials'][$iterator]['transportation_igst_percentage']}}%)
+                                    @php
+                                        $totalIGSTAmount += $vendorInfo['materials'][$iterator]['transportation_igst_amount'];
+                                    @endphp
                                 </td>
                                 <td>
                                     {{$vendorInfo['materials'][$iterator]['transportation_total_amount']}}
+                                    @php
+                                        $grandTotal += $vendorInfo['materials'][$iterator]['transportation_total_amount'];
+                                    @endphp
                                 </td>
                             </tr>
                             @endif
                         @endfor
-                        @for($i = 0;$i < (12-(count($vendorInfo['materials'])));$i++)
+                        @for($i = 0;$i < (11-(count($vendorInfo['materials'])));$i++)
                             <tr style="text-align: center">
                                 <td>
                                 </td>
@@ -251,6 +294,7 @@
                                     <td style="text-align: left;padding-left: 5px" >
 
                                     </td>
+
                                     <td>
 
                                     </td>
@@ -288,6 +332,50 @@
                                 @endif
                             </tr>
                         @endfor
+                        <tr style="text-align: center">
+                            <td>
+                            </td>
+                            @if(isset($pdfFlag) && ($pdfFlag == 'after-purchase-order-create' || $pdfFlag == 'purchase-order-listing-download'))
+                                <td style="text-align: left;padding-left: 5px" >
+                                    Total
+                                </td>
+
+                                <td>
+                                    {{$totalQuantity}}
+                                </td>
+                                <td>
+
+                                </td>
+                                <td>
+                                    {{$totalRate}}
+                                </td>
+                                <td>
+                                    {{$totalSubtotal}}
+                                </td>
+                                <td>
+                                    {{$totalCGSTAmount}}
+                                </td>
+                                <td>
+                                    {{$totalSGSTAmount}}
+                                </td>
+                                <td>
+                                    {{$totalIGSTAmount}}
+                                </td>
+                                <td>
+                                    {{$grandTotal}}
+                                </td>
+                            @else
+                                <td style="text-align: left;padding-left: 5px" >
+                                    Total
+                                </td>
+                                <td>
+                                    {{$totalQuantity}}
+                                </td>
+                                <td>
+
+                                </td>
+                            @endif
+                        </tr>
                     </table>
                 </td>
             </tr>
