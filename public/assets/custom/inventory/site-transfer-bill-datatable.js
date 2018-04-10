7c96b21d3494de42d3b154b61baa803dca9e757c
harsha <harsha.woxi@gmail.com>
2018-04-10 14:15:41 +0530

    inventory total done

--- public/assets/custom/inventory/site-transfer-bill-datatable.js
+++ public/assets/custom/inventory/site-transfer-bill-datatable.js
@@ -19,7 +19,76 @@ var SiteTransferBillListing = function () {
                 // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/scripts/datatable.js).
                 // So when dropdowns used the scrollable div should be removed.
                 //"dom": "<'row'<'col-md-8 col-sm-12'pli><'col-md-4 col-sm-12'<'table-group-actions pull-right'>>r>t<'row'<'col-md-8 col-sm-12'pli><'col-md-4 col-sm-12'>>",
+                "footerCallback": function ( row, data, start, end, display ) {
+                    console.log('inside here');
+                    var api = this.api(), data;
 
+                    // Remove the formatting to get integer data for summation
+                    var intVal = function ( i ) {
+                        return typeof i === 'string' ?
+                            i.replace(/[\$,]/g, '')*1 :
+                            typeof i === 'number' ?
+                                i : 0;
+                    };
+
+                    var vendor_name = $('#vendor_name').val();
+
+
+                    // Total over all pages
+                    $.ajax({
+                        url: "/inventory/transfer/billing/listing?_token="+$("input[name='_token']").val(), // ajax source
+                        type: 'POST',
+                        data :{
+                            "get_total" : true,
+                            "vendor_name" : vendor_name
+                        },
+                        success: function(result){
+                            total = result['total'];
+
+                            // Total over this page
+                            pageTotal = api
+                                .column( 8, { page: 'current'} )
+                                .data()
+                                .reduce( function (a, b) {
+                                    return intVal(a) + intVal(b);
+                                }, 0 );
+
+                            // Update footer
+                            $( api.column( 8 ).footer() ).html(
+                                pageTotal +' ( '+ total +' total)'
+                            );
+
+                            billtotal = result['billtotal'];
+
+                            // Total over this page
+                            pageBillTotal = api
+                                .column( 9, { page: 'current'} )
+                                .data()
+                                .reduce( function (a, b) {
+                                    return intVal(a) + intVal(b);
+                                }, 0 );
+
+                            // Update footer
+                            $( api.column( 9 ).footer() ).html(
+                                pageBillTotal +' ( '+ billtotal +' total)'
+                            );
+
+                            paidtotal = result['paidtotal'];
+
+                            // Total over this page
+                            pagePaidTotal = api
+                                .column( 10, { page: 'current'} )
+                                .data()
+                                .reduce( function (a, b) {
+                                    return intVal(a) + intVal(b);
+                                }, 0 );
+
+                            // Update footer
+                            $( api.column( 10 ).footer() ).html(
+                                pagePaidTotal +' ( '+ paidtotal +' total)'
+                            );
+                        }});
+                },
                 "lengthMenu": [
                     [50, 100, 150],
                     [50, 100, 150] // change per page values here
