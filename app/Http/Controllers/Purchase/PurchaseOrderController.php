b6bea3d11a2961b8a85df9e26193a2c7df6ec758
harsha <harsha.woxi@gmail.com>
2018-06-21 11:57:49 +0530

    done

--- app/Http/Controllers/Purchase/PurchaseOrderController.php
+++ app/Http/Controllers/Purchase/PurchaseOrderController.php
@@ -208,12 +208,10 @@ class PurchaseOrderController extends Controller
 
             if ($vendor_name != "" & $filterFlag == true) {
                 $forVendorSearchIds = PurchaseOrder::join('vendors','vendors.id','=','purchase_orders.vendor_id')
-                    ->join('clients','clients.id','=','purchase_orders.vendor_id')
-                    ->where('vendors.company','ilike','%'.$vendor_name.'%')->orWhere('vendors.company','ilike','%'.$vendor_name.'%')->whereIn('purchase_orders.id',$ids)->pluck('purchase_orders.id');
+                    ->where('vendors.company','ilike','%'.$vendor_name.'%')->whereIn('purchase_orders.id',$ids)->pluck('purchase_orders.id')->toArray();
                 $forClientSearchIds = PurchaseOrder::join('clients','clients.id','=','purchase_orders.client_id')
-                    ->join('clients','clients.id','=','purchase_orders.client_id')
-                    ->where('clients.company','ilike','%'.$vendor_name.'%')->orWhere('clients.company','ilike','%'.$vendor_name.'%')->whereIn('purchase_orders.id',$ids)->pluck('purchase_orders.id');
-                $ids = array_merge($forVendorSearchIds,$forClientSearchIds);
+                    ->where('clients.company','ilike','%'.$vendor_name.'%')->whereIn('purchase_orders.id',$ids)->pluck('purchase_orders.id')->toArray();
+                $ids = array_merge($forVendorSearchIds , $forClientSearchIds);
                 if(count($ids) <= 0) {
                     $filterFlag = false;
                 }
