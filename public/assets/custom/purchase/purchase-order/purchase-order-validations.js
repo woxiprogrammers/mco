d253919fdba20fe8020f347af5ada9d7fcb5a2d7
Ameya Joshi <ameya.woxi@gmail.com>
2018-07-12 18:36:13 +0530

    grn double click issue resolved

--- public/assets/custom/purchase/purchase-order/purchase-order-validations.js
+++ public/assets/custom/purchase/purchase-order/purchase-order-validations.js
@@ -199,3 +199,57 @@ var  EditPurchaseOrder = function () {
         }
     };
 }();
+
+
+var  GenerateGRN = function () {
+    var handleCreate = function() {
+        var form = $('#transactionForm');
+        var error = $('.alert-danger', form);
+        var success = $('.alert-success', form);
+        form.validate({
+            errorElement: 'span', //default input error message container
+            errorClass: 'help-block', // default input error message class
+            focusInvalid: false, // do not focus the last invalid input
+            /*rules: {
+                user_name: {
+                    required: true
+                },
+                user_id:{
+                    requried: true
+                }
+            },
+            messages: {
+                user_name:{
+                    required: 'Please select the user.'
+                }
+            },*/
+            invalidHandler: function (event, validator) { //display error alert on form submit
+                success.hide();
+                error.show();
+            },
+            highlight: function (element) { // hightlight error inputs
+                $(element)
+                    .closest('.form-group').addClass('has-error'); // set error class to the control group
+            },
+            unhighlight: function (element) { // revert the change done by hightlight
+                $(element)
+                    .closest('.form-group').removeClass('has-error'); // set error class to the control group
+            },
+            success: function (label) {
+                label
+                    .closest('.form-group').addClass('has-success');
+            },
+            submitHandler: function (form) {
+                    $("button[type='submit']").prop('disabled', true);
+                    success.show();
+                    error.hide();
+                    form.submit();
+            }
+        });
+    }
+    return {
+        init: function () {
+            handleCreate();
+        }
+    };
+}();
