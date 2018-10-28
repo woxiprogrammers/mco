5c2c6cf6d3f4af09e1b8c59d01234785604623c3
Ameya Joshi <joshiameya1993@gmail.com>
2018-10-28 17:42:36 +0530

    Migration, seeder and models completed for new subcontractor bill module

---
+++ app/SubcontractorStructureSummary.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubcontractorStructureSummary extends Model
+{
+    protected $table = 'subcontractor_structure_summaries';
+
+    protected $fillable = ['subcontractor_structure_id', 'rate', 'total_work_area', 'description', 'summary_id'];
+
+    public function subcontractorStructure(){
+        return $this->belongsTo('App\SubcontractorStructure', 'subcontractor_structure_id');
+    }
+
+    public function summary(){
+        return $this->belongsTo('App\Summary', 'summary_id');
+    }
+
+    public function subcontractorBillSummaries(){
+        return $this->hasMany('App\SubcontractorBillSummary', 'subcontractor_structure_summary_id');
+    }
+}
---
+++ database/migrations/2018_10_28_164545_add_transaction_status_payment_mode_columns_into_subcontractor_bill_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTransactionStatusPaymentModeColumnsIntoSubcontractorBillTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subcontractor_bill_transactions', function (Blueprint $table) {
+            $table->unsignedInteger('transaction_status_id')->nullable();
+            $table->foreign('transaction_status_id')->references('id')->on('transaction_statuses')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('payment_mode_slug', 255)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subcontractor_bill_transactions', function (Blueprint $table) {
+            $table->dropForeign('subcontractor_bill_transactions_transaction_status_id_foreign');
+            $table->dropColumn('transaction_status_id');
+            $table->dropColumn('payment_mode_slug');
+        });
+    }
+}
