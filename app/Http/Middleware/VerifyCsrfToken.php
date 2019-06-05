7c81cc7b14f13140d43aa0f180f31c339a657d2b
Bharat Makwana <bharat.woxi@gmail.com>
2019-06-05 19:30:09 +0530

    po close script run

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,11 +12,11 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        'purchase/purchase-order-bill/listing','/inventory/transfer/listing','/inventory/transfer/challen-generation','/subcontractor/bill/transaction/listing','/subcontractor/structure/listing','/material/check-name','/material/listing','/category/listing','/product/listing','/profit-margin/listing','/units/listing','/units/conversion/listing',
+        '/purchase/purchase-order/transaction/check-quantity','/purchase/purchase-order/get-component-details','/purchase/purchase-order/listing','purchase/purchase-order-bill/listing','/inventory/transfer/listing','/inventory/transfer/challen-generation','/subcontractor/bill/transaction/listing','/subcontractor/structure/listing','/material/check-name','/material/listing','/category/listing','/product/listing','/profit-margin/listing','/units/listing','/units/conversion/listing',
         '/summary/listing','/tax/listing','/quotation/get-materials','/quotation/get-profit-margins','/quotation/listing/*','bill/create','/project/listing','/quotation/get-product-calculations',
         '/quotation/image-upload/*','/quotation/display-images/*','/quotation/delete-temp-product-image','bill/approve','/bill/image-upload/*','/bill/display-images/*','/bill/delete-temp-product-image','/bill/edit/*',
         '/bill/product_description/create','/bill/product_description/update','/bill/calculate-tax-amounts','/bill/transaction/listing/*',
-         '/quotation/check-product-remove','/extra-item/listing','/vendors/listing','/bank/listing','/checklist/category-management/listing','purchase/material-request/get-materials','/purchase/material-request/get-units',
+        '/quotation/check-product-remove','/extra-item/listing','/vendors/listing','/bank/listing','/checklist/category-management/listing','purchase/material-request/get-materials','/purchase/material-request/get-units',
         'purchase/material-request/create','/checkList/listing','/checklist/category-management/listing','role/listing',
         '/user/project-site/auto-suggest/*','/asset/listing','/asset/display-images','/asset/delete-temp-product-image','purchase/purchase-order/get-details','purchase/purchase-order/create-transaction','purchase/purchase-order/add-payment',
         'labour/listing','/peticash/master-peticash-account/listing','/peticash/sitewise-peticash-account/listing', '/peticash/peticash-approval-request/manage-purchase-list-ajax','/peticash/peticash-approval-request/manage-salary-list-ajax',
