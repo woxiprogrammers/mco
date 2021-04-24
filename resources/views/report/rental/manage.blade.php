70246bdccdb7251dd2b83df2c8418e9d29d54bad
Harsha Chotaliya <harsha@geospoc.com>
2021-04-24 16:25:51 +0530

    feat: added yearly calculations

--- resources/views/report/rental/manage.blade.php
+++ resources/views/report/rental/manage.blade.php
@@ -68,11 +68,11 @@
                                                             </select></th>
                                                         <th><select class="form-control form-filter" id="year" name="year">
                                                                 <option value="0">ALL</option>
+                                                                <option value="2018">2018</option>
+                                                                <option value="2019">2019</option>
+                                                                <option value="2020">2020</option>
                                                                 <option value="2021">2021</option>
                                                                 <option value="2022">2022</option>
-                                                                <option value="2023">2023</option>
-                                                                <option value="2024">2024</option>
-                                                                <option value="2025">2025</option>
                                                             </select></th>
                                                         <th><input class="form-control form-filter" name="bill_number" id="bill_number" type="text"></th>
                                                         <th></th>
