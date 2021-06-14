abc0932bf3c86e4e5b6e15a6835f30c80006427d
Sagar Acharya <sagar_acharya@live.in>
2021-06-14 17:36:12 +0530

    reverted logic

--- database/seeds/DeleteZombieICTGrn.php
+++ database/seeds/DeleteZombieICTGrn.php
@@ -16,21 +16,16 @@ class DeleteZombieICTGrn extends Seeder
     public function run()
     {
         // $potGrn = PurchaseOrderTransaction::pluck('grn');
-        /*$potBackupGrn = PurchaseOrderTransactionBackup::pluck('grn');
+        $potBackupGrn = PurchaseOrderTransactionBackup::pluck('grn');
         $ictGrn = InventoryComponentTransfers::whereIn('grn',$potBackupGrn)->get();
 
         foreach($ictGrn as $ict) {
             InventoryComponentTransfersBackup::create($ict->toArray());
             $ict->delete();
-        }*/
-        //$ictGrn = InventoryComponentTransfers::leftJoin('purchase_order_transactions','purchase_order_transactions.grn','=','inventory_component_transfers.grn')
-        //            ->where('purchase_order_transactions.grn','!=','inventory_component_transfers.grn')->get();
-        // $potBackupGrn = PurchaseOrderTransaction::whereNotIn('grn',$ictGrn)->count();
-        /*foreach($ictGrn as $grn) {
-            dd($grn);
-        }*/
+        }
+        
 
-        $ictGrn = InventoryComponentTransfers::pluck('grn');
+        /*$ictGrn = InventoryComponentTransfers::pluck('grn');
         $potGrn = PurchaseOrderTransaction::pluck('grn');
         $potGrn = $potGrn->toArray();
         $grnMaster = [];
@@ -44,6 +39,6 @@ class DeleteZombieICTGrn extends Seeder
         foreach($inventoryComponents as $inventory) {
             InventoryComponentTransfersBackup::create($inventory->toArray());
             $inventory->delete();
-        }
+        }*/
     }
 }
